13ebf601482dcc7a314f6d57dd8723ff5b7d82a5
Swantje Moehle <s.moehle@alumni.ubc.ca>
2023-10-16 10:26:19 -0700

    added plant spacing (sfg) to plant_info model and added admin views for seed inventory

--- mysite/garden_planner/admin.py
+++ mysite/garden_planner/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Plant_info, Seed_inventory
+from .models import Plant_info, Seed_inventory, Plant_families
 
 
 class Plant_infoAdmin(admin.ModelAdmin):
-    list_display = ("plant_name", "family_name")
+    list_display = ("plant_name", "family_name", "plants_per_sqf")
+
+
+class Seed_inventoryAdmin(admin.ModelAdmin):
+    list_display = ("seed_name", "plant_name", "seed_source", "seed_year")
 
 
 # admin.site.register(Plant_info)
 admin.site.register(Plant_info, Plant_infoAdmin)
-admin.site.register(Seed_inventory)
+admin.site.register(Seed_inventory, Seed_inventoryAdmin)
+admin.site.register(Plant_families)
--- mysite/garden_planner/models.py
+++ mysite/garden_planner/models.py
@@ -9,28 +9,34 @@ from django.utils import timezone
 class Plant_families(models.Model):
     family_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.family_name
+
 
 class Plant_info(models.Model):
     plant_name = models.CharField(max_length=200)
     family_name = models.ForeignKey(Plant_families, on_delete=models.PROTECT, null=True)
     #   plant_family = models.CharField(max_length=200, blank=True, null=True, default="")
     YVR_in_seedDate_start = models.DateField(
-        "Earliest indoor seed starting date", blank=True, null=True
+        "Earliest indoor seed date", blank=True, null=True
     )
     YVR_in_seedDate_stop = models.DateField(
-        "Latest indoor seed starting date", blank=True, null=True
+        "Latest indoor seed date", blank=True, null=True
     )
     YVR_out_seedDate_start = models.DateField(
-        "Earliest outdoor seed starting date", blank=True, null=True
+        "Earliest outdoor seed date", blank=True, null=True
     )
     YVR_out_seedDate_stop = models.DateField(
-        "Latest outdoor seed starting date", blank=True, null=True
+        "Latest outdoor seed date", blank=True, null=True
     )
     YVR_tpDate_start = models.DateField(
         "Earliest transplant date", blank=True, null=True
     )
     YVR_tpDate_stop = models.DateField("Latest transplant date", blank=True, null=True)
     pub_date = models.DateField("date added to DB")
+    plants_per_sqf = models.SmallIntegerField(
+        "Plants per square foot", blank=True, null=True
+    )
 
     def __str__(self):
         return self.plant_name
