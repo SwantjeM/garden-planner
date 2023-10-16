8a279196e61dde2168f662b8d9b2b78abe242c4d
Swantje Moehle <s.moehle@alumni.ubc.ca>
2023-10-16 11:01:24 -0700

    added notes and plant type and dimensions to seed inventory model.

--- mysite/garden_planner/models.py
+++ mysite/garden_planner/models.py
@@ -47,12 +47,16 @@ class Plant_info(models.Model):
 
 class Seed_inventory(models.Model):
     seed_name = models.CharField(max_length=200)
+    plant_name = models.ForeignKey(Plant_info, on_delete=models.PROTECT)
+    size_type = models.CharField(max_length=200, default="", blank=True)
+    seed_year = models.PositiveSmallIntegerField(default=timezone.now().year)
     seed_source = models.CharField(max_length=200, default="", blank=True)
     seed_link = models.CharField(max_length=1000, default="", blank=True)
-    seed_year = models.PositiveSmallIntegerField(default=timezone.now().year)
+    height_cm = models.PositiveSmallIntegerField(blank=True, null=True)
+    width_cm = models.PositiveSmallIntegerField(blank=True, null=True)
     pub_date = models.DateField("date added to DB")
-    plant_name = models.ForeignKey(Plant_info, on_delete=models.PROTECT)
     rating = models.IntegerField(default=0)
+    seed_notes = models.TextField(default="", blank=True)
 
     def __str__(self):
         return self.seed_name
